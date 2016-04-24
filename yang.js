18d89b14aaa7966da1a55095269e3036503b620c
eritikass <eritikass@gmail.com>
2016-04-24 20:06:14 +0300

    number format

--- yang.js
+++ yang.js
@@ -81,7 +81,7 @@
     /**
      * @type {string}
      */
-    var FOOTER = '© 2016 transferYang | <a target="_blank" href="https://github.com/eritikass/transferYang"><svg aria-hidden="true" height="12" width="12" version="1.1" viewBox="0 0 16 16"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59 0.4 0.07 0.55-0.17 0.55-0.38 0-0.19-0.01-0.82-0.01-1.49-2.01 0.37-2.53-0.49-2.69-0.94-0.09-0.23-0.48-0.94-0.82-1.13-0.28-0.15-0.68-0.52-0.01-0.53 0.63-0.01 1.08 0.58 1.23 0.82 0.72 1.21 1.87 0.87 2.33 0.66 0.07-0.52 0.28-0.87 0.51-1.07-1.78-0.2-3.64-0.89-3.64-3.95 0-0.87 0.31-1.59 0.82-2.15-0.08-0.2-0.36-1.02 0.08-2.12 0 0 0.67-0.21 2.2 0.82 0.64-0.18 1.32-0.27 2-0.27 0.68 0 1.36 0.09 2 0.27 1.53-1.04 2.2-0.82 2.2-0.82 0.44 1.1 0.16 1.92 0.08 2.12 0.51 0.56 0.82 1.27 0.82 2.15 0 3.07-1.87 3.75-3.65 3.95 0.29 0.25 0.54 0.73 0.54 1.48 0 1.07-0.01 1.93-0.01 2.2 0 0.21 0.15 0.46 0.55 0.38C13.71 14.53 16 11.53 16 8 16 3.58 12.42 0 8 0z"></path></svg></a>';
+    var FOOTER = '© 2016 transferYang <!-- | <a target="_blank" href="https://github.com/eritikass/transferYang"><svg aria-hidden="true" height="12" width="12" version="1.1" viewBox="0 0 16 16"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59 0.4 0.07 0.55-0.17 0.55-0.38 0-0.19-0.01-0.82-0.01-1.49-2.01 0.37-2.53-0.49-2.69-0.94-0.09-0.23-0.48-0.94-0.82-1.13-0.28-0.15-0.68-0.52-0.01-0.53 0.63-0.01 1.08 0.58 1.23 0.82 0.72 1.21 1.87 0.87 2.33 0.66 0.07-0.52 0.28-0.87 0.51-1.07-1.78-0.2-3.64-0.89-3.64-3.95 0-0.87 0.31-1.59 0.82-2.15-0.08-0.2-0.36-1.02 0.08-2.12 0 0 0.67-0.21 2.2 0.82 0.64-0.18 1.32-0.27 2-0.27 0.68 0 1.36 0.09 2 0.27 1.53-1.04 2.2-0.82 2.2-0.82 0.44 1.1 0.16 1.92 0.08 2.12 0.51 0.56 0.82 1.27 0.82 2.15 0 3.07-1.87 3.75-3.65 3.95 0.29 0.25 0.54 0.73 0.54 1.48 0 1.07-0.01 1.93-0.01 2.2 0 0.21 0.15 0.46 0.55 0.38C13.71 14.53 16 11.53 16 8 16 3.58 12.42 0 8 0z"></path></svg></a>-->';
 
     /**
      * get amazon url
@@ -319,7 +319,7 @@
                 + '<td class="pricechecked_amazon_rr1"><strong class="pricechecked_amazon_name">' + name + '</strong></td>'
                 + '<td class="pricechecked_amazon_rr2 pricechecked_amazon_numbcol">' + ( pricedata.code != 'EUR' ? [pricedata.code, formatNumber(pricedata.price)].join(' ') : '&nbsp;') + '</td>'
                 + '<td class="pricechecked_amazon_rr3 pricechecked_amazon_numbcol">EUR ' + formatNumber(eur) + '</td>'
-                + '<td class="pricechecked_amazon_rr4 pricechecked_amazon_numbcol ' + (priceDif < 0 ? 'pricechecked_amazon_numbcolpos' : (priceDif > 0 ? 'pricechecked_amazon_numbcolneg' : '')) + '">' + (priceDif > 0 ? '+' : '') + formatNumber(priceDif) + '</td>'
+                + '<td class="pricechecked_amazon_rr4 pricechecked_amazon_numbcol ' + (priceDif < 0 ? 'pricechecked_amazon_numbcolpos' : (priceDif > 0 ? 'pricechecked_amazon_numbcolneg' : '')) + '">' + formatNumber(Math.abs(priceDif)) + '</td>'
                 + '</tr>';
 
             var $tb = $('<table class="pricechecked_amazon_tb' + (host == document.location.hostname ? ' pricechecked_amazon_curhost' : '') + '">' + htmlTb + '</table>');
