2b8a0f4bbaf2eb9b7c387f92b812d9db16d86c86
eritikass <eritikass@gmail.com>
2016-04-25 01:26:43 +0300

    more currency rates fetch to background.js

--- background.js
+++ background.js
@@ -1,10 +1,57 @@
 // make amazon requests in background to avoid mixed content errors
 
+var manifestData = chrome.app.getDetails();
+
+var fixerJsonUrl = "https://api.fixer.io/latest";
+
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
+
+        if (request && request.message === "yang_push_version") {
+            chrome.tabs.sendMessage(sender.tab.id, {
+                "message": "yang_display_version",
+                "version": manifestData.version
+            });
+
+        }
+
+        if (request && request.message === "yang_fixerdata_push" && request.callback_name) {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", fixerJsonUrl, true);
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4) {
+
+                    var data = xhr.responseText || "";
+                    var rates = {};
+
+                    if (data && (typeof data == 'string')) {
+                        data = JSON.parse(data);
+                    }
+
+                    if (data.rates && data.base == 'EUR') {
+                        for (var ccode in data.rates) {
+                            var rate = data.rates[ccode];
+                            if (rate && (typeof rate == "number") && (typeof ccode == "string") ) {
+                                ccode = ccode.toUpperCase();
+                                rates[[ccode, 'EUR'].join('-')] = {
+                                    source: ccode,
+                                    target: 'EUR',
+                                    rate: 1 / rate
+                                }
+                            }
+                        }
+                    }
+                    chrome.tabs.sendMessage(sender.tab.id, {
+                        "message": request.callback_name,
+                        "data": rates
+                    });
+                }
+            }
+            xhr.send();
+        }
+
         if (request && request.message === "yang_request_amazon" && request.site && request.asin) {
             var amazon_url = "https://" + request.site + "/dp/" + request.asin + "/";
-            console.log('yang_request_amazon', request.site, request.asin, amazon_url);
 
             var xhr = new XMLHttpRequest();
             xhr.open("GET", amazon_url, true);
--- yang.js
+++ yang.js
@@ -26,7 +26,7 @@
      *
      * @type {string}
      */
-    var CURRENCY_FEED = 'fixer.io';
+    var CURRENCY_FEED = 'api.fixer.io';
 
     /**
      * fetch amazon data in background
@@ -81,7 +81,10 @@
     /**
      * @type {string}
      */
-    var FOOTER = '© 2016 transferYang <!-- | <a target="_blank" href="https://github.com/eritikass/transferYang"><svg aria-hidden="true" height="12" width="12" version="1.1" viewBox="0 0 16 16"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59 0.4 0.07 0.55-0.17 0.55-0.38 0-0.19-0.01-0.82-0.01-1.49-2.01 0.37-2.53-0.49-2.69-0.94-0.09-0.23-0.48-0.94-0.82-1.13-0.28-0.15-0.68-0.52-0.01-0.53 0.63-0.01 1.08 0.58 1.23 0.82 0.72 1.21 1.87 0.87 2.33 0.66 0.07-0.52 0.28-0.87 0.51-1.07-1.78-0.2-3.64-0.89-3.64-3.95 0-0.87 0.31-1.59 0.82-2.15-0.08-0.2-0.36-1.02 0.08-2.12 0 0 0.67-0.21 2.2 0.82 0.64-0.18 1.32-0.27 2-0.27 0.68 0 1.36 0.09 2 0.27 1.53-1.04 2.2-0.82 2.2-0.82 0.44 1.1 0.16 1.92 0.08 2.12 0.51 0.56 0.82 1.27 0.82 2.15 0 3.07-1.87 3.75-3.65 3.95 0.29 0.25 0.54 0.73 0.54 1.48 0 1.07-0.01 1.93-0.01 2.2 0 0.21 0.15 0.46 0.55 0.38C13.71 14.53 16 11.53 16 8 16 3.58 12.42 0 8 0z"></path></svg></a>-->';
+    var FOOTER = '<span class="pricechecked_amazon_plugin_title">© 2016 transferYang</span>'
+            + '<span class="pricechecked_amazon_plugin_version"></span>'
+       // + ' | <a target="_blank" href="https://github.com/eritikass/transferYang"><svg aria-hidden="true" height="12" width="12" version="1.1" viewBox="0 0 16 16"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59 0.4 0.07 0.55-0.17 0.55-0.38 0-0.19-0.01-0.82-0.01-1.49-2.01 0.37-2.53-0.49-2.69-0.94-0.09-0.23-0.48-0.94-0.82-1.13-0.28-0.15-0.68-0.52-0.01-0.53 0.63-0.01 1.08 0.58 1.23 0.82 0.72 1.21 1.87 0.87 2.33 0.66 0.07-0.52 0.28-0.87 0.51-1.07-1.78-0.2-3.64-0.89-3.64-3.95 0-0.87 0.31-1.59 0.82-2.15-0.08-0.2-0.36-1.02 0.08-2.12 0 0 0.67-0.21 2.2 0.82 0.64-0.18 1.32-0.27 2-0.27 0.68 0 1.36 0.09 2 0.27 1.53-1.04 2.2-0.82 2.2-0.82 0.44 1.1 0.16 1.92 0.08 2.12 0.51 0.56 0.82 1.27 0.82 2.15 0 3.07-1.87 3.75-3.65 3.95 0.29 0.25 0.54 0.73 0.54 1.48 0 1.07-0.01 1.93-0.01 2.2 0 0.21 0.15 0.46 0.55 0.38C13.71 14.53 16 11.53 16 8 16 3.58 12.42 0 8 0z"></path></svg></a>'
+        + '';
 
     /**
      * get amazon url
@@ -140,6 +143,20 @@
      * @param {function} callback
      */
     function getRates(callback) {
+
+        if (CURRENCY_FEED == 'api.fixer.io') {
+            var callback_name = "yang_apifixerio_" + $.now();
+            chrome.runtime.sendMessage({"message": "yang_fixerdata_push", "callback_name": callback_name});
+            chrome.runtime.onMessage.addListener(
+                function (request) {
+                    if (request.message === callback_name) {
+                        rates = request.data || {};
+                        callback();
+                    }
+                }
+            );
+            return
+        }
 
         if (CURRENCY_FEED == 'fixer.io') {
             $.getJSON('//api.fixer.io/latest', function (data) {
@@ -398,6 +415,8 @@
             .append($("<span></span>").addClass('pricechecked_amazon_copyr').html(FOOTER))
             .append($hideshow)
             .css('height', height + 'px');
+
+        chrome.runtime.sendMessage({"message": "yang_push_version"});
     }
 
 
@@ -428,6 +447,15 @@
 
     // list price
     addKnownPrice(document.location.hostname, price)
+
+
+    chrome.runtime.onMessage.addListener(
+        function (request) {
+            if (request.message === "yang_display_version") {
+                $('.pricechecked_amazon_plugin_version').html(', v' + request.version);
+            }
+        }
+    );
 
     var busyFetching = {};
     if (AMAZON_BG_FETCH) {
